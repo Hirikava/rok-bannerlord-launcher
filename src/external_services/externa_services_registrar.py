95aed982aa5c1a84312d6fc598b52fbe028b54bb
daas <daas@skbkontur.ru>
2026-05-07 22:50:07 +0500

    support new update

--- src/external_services/externa_services_registrar.py
+++ src/external_services/externa_services_registrar.py
@@ -1,3 +1,5 @@
+import os
+
 import punq
 
 from src.external_services.rok_packages_service.rok_package_service_factory import RokPackageServiceFactory
@@ -10,5 +12,12 @@ class ExternalServicesRegistrar:
         container.register(
             RokPackageServiceFactory,
             instance=RokPackageServiceFactory(
-                "rokbannerlord.ru",
+                ExternalServicesRegistrar.__get_rokb_server_addr(),
                 7272))
+
+    @staticmethod
+    def __get_rokb_server_addr():
+        is_stage = os.getenv('ROKB_STAGE_ENV', default=None)
+        if is_stage is None:
+            return "rokbannerlord.ru"
+        return "127.0.0.1"
--- src/presentation/launcher_statets/ready_for_launch_launcher_state.py
+++ src/presentation/launcher_statets/ready_for_launch_launcher_state.py
@@ -5,17 +5,21 @@ from pathlib import Path
 import imgui
 
 from src.domain.launcher_context import LauncherContext
+from src.infrastructure.repositories.local_version_repository.local_version_repository import LocalVersionRepository
 from src.presentation.launcher_statets.launcher_state_base import LauncherStateBase
 from src.utils.system_utils import SystemUtils
 
 
 class ReadyForLaunchLauncherState(LauncherStateBase):
     launcher_context: LauncherContext
+    local_version_repo: LocalVersionRepository
 
     def __init__(
             self,
-            launcher_context: LauncherContext):
+            launcher_context: LauncherContext,
+            local_version_repo: LocalVersionRepository):
         self.launcher_context = launcher_context
+        self.local_version_repo = local_version_repo
 
     async def on_init(self):
         pass
@@ -26,16 +30,18 @@ class ReadyForLaunchLauncherState(LauncherStateBase):
             # TODO TO INTERNAL COMMAND
             adb_path = Path.cwd() / "adb-platform-tools"
 
-            rokb_exe_path = Path.cwd() / "rok-bannerlord-client" / "rok_bannerlord_client.exe"
+            rokb_agent_exe_path = Path.cwd() / "rok-bannerlord-agent" / "rok_bannerlord_agent.exe"
+            rokb_client_exe_path = Path.cwd() / "rok-bannerlord-client" / "rok_bannerlord_client.exe"
             rokb_server_address = self.__get_rokb_server_addr()
 
+            client_package = await self.local_version_repo.get_package_current_version("rok-bannerlord-client")
+
             os.environ["PATH"] += os.pathsep + adb_path.__str__()
-            os.environ["ROKB_WORKDIR"] = Path.cwd().__str__()
-            os.environ["ROKB_ASSETSDIR"] = (Path.cwd() / "rok-bannerlord-agent" / "assets").__str__()
-            os.environ["ROKB_APP_VERSION"] = "1.0.0"
+            os.environ["ROKB_ASSET_PATH"] = (Path.cwd() / "rok-bannerlord-agent" / "assets").__str__()
+            os.environ["ROKB_APP_VERSION"] = client_package.package_version
             os.environ["ROKB_CLIENT_PORT"] = str(SystemUtils.get_free_port())
             os.environ["ROKB_CLIENT_HOST"] = "127.0.0.1"
-            os.environ["ROKB_EXE_PATH"] = str(rokb_exe_path)
+            os.environ["ROKB_EXE_PATH"] = str(rokb_agent_exe_path)
             os.environ["ROKB_LOCAL_DATA_DIR"] = str(self.__get_path_to_local_app_data())
             os.environ["ROKB_RUN_MODE"] = "BINARY"
             os.environ["ROKB_SERVER_API_KEY"] = self.launcher_context.user_api_key
@@ -43,7 +49,7 @@ class ReadyForLaunchLauncherState(LauncherStateBase):
             os.environ["ROKB_SERVER_CONNECTION_ADDR"] = rokb_server_address
 
             process = subprocess.Popen(
-                [rokb_exe_path.__str__()],
+                [rokb_client_exe_path.__str__()],
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
                 stdin=subprocess.DEVNULL,
